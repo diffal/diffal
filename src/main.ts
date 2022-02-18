07ad8fbf3268d098dbc2af69eb1447d44a035491
mostafa <mostafa8026@gmail.com>
2022-02-18 20:35:59 +0330

    feat(frontend): add public assets
    for favicon.ic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, 'public'));
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 }
