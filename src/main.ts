188ac67dd924da5065d7c6b2b11a4112229477c5
mostafa <mostafa8026@gmail.com>
2022-02-18 19:10:27 +0330

    fix(devops): fix PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 }
 bootstrap();
