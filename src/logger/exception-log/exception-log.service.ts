4a886c47c98dd139f44a20772c9bb69ad4a5e817
JafarFahimi <fahimi85@gmail.com>
2022-02-26 17:05:23 +0330

    fix(backend): bugfix exception filter that log all http exceptions

--- src/logger/exception-log/exception-log.service.ts
+++ src/logger/exception-log/exception-log.service.ts
@@ -3,7 +3,9 @@ import { ExceptionLogRepository } from '../entities/exception-log.repository';
 
 @Injectable()
 export class ExceptionLogService {
-  constructor(private readonly exceptionLogRepository: ExceptionLogRepository) { }
+  constructor(
+    private readonly exceptionLogRepository: ExceptionLogRepository,
+  ) {}
 
   insertLog(statusCode: number, message: string) {
     const log = this.exceptionLogRepository.create({
