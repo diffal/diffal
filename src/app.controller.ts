e6b6ea65f711f4b1d7d48abc3d0d2ab8b279f534
JafarFahimi <fahimi85@gmail.com>
2022-02-28 22:39:50 +0330

    feat(backend):issue-11-We_need_a_simple_management_panel

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Res } from '@nestjs/common';
+import { Response } from 'express';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  Root(@Res() res: Response) {
+
+    return res.render('index', {
+      title: 'دیفال',
+    }
+    )
+  }
+
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', '/views/partials'));
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 }
