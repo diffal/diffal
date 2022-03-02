4e72ed42d5284245a43a08e99b09415249c9da35
mostafa <mostafa8026@gmail.com>
2022-03-02 16:47:09 +0330

    feat(frontend): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Our Api Specification')
+    .setVersion('1.0.0')
+    .setDescription('Our Api Description')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/docs', app, document);
+
   app.useStaticAssets(join(__dirname, 'public'));
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => console.log(`Listening on ${PORT}`));
