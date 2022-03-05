5eb98fb3501619055277fef558e94d84e3667c51
JafarFahimi <fahimi85@gmail.com>
2022-03-05 10:48:59 +0330

    test(backend): set test for log-request.service

--- src/log-request/log-request.module.ts
+++ src/log-request/log-request.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LogRequestService } from './log-request.service';
-import { LogRequestController } from './log-request.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LogRequestRepository } from './entities/log-request.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([LogRequestRepository])],
-  controllers: [LogRequestController],
   providers: [LogRequestService],
   exports: [LogRequestService],
 })
-export class LogRequestModule {}
+export class LogRequestModule { }
