bd0e5e3933fae32ee1fe0f799462a2a1e6f1f7e0
Rzix <wonderword2002@gmail.com>
2022-03-04 21:48:04 -0800

    feat(backend): add delete & find for  category

---
+++ src/categories/dto/pageinated_category.ts
@@ -0,0 +1,11 @@
+import { Type } from "class-transformer";
+import { IsOptional } from "class-validator";
+
+export class PaginatedDto {
+    @Type(()=>Number )
+    @IsOptional()
+    page:number;
+    @Type(()=>Number)
+    @IsOptional()
+    page_count:number;
+}
