b8cfe8292495260f72167359683b1a10b2824573
mostafa <mostafa8026@gmail.com>
2022-03-11 20:17:06 +0330

    fix(backend): fix category child remove

--- src/categories/entities/category.Repository.ts
+++ src/categories/entities/category.Repository.ts
@@ -1,5 +1,5 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, TreeRepository } from 'typeorm';
 import { CategoryEntity } from './category.entity';
 
 @EntityRepository(CategoryEntity)
-export class CategoryRepository extends Repository<CategoryEntity> {}
+export class CategoryRepository extends TreeRepository<CategoryEntity> {}
